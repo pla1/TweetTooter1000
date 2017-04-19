082a935332e396fb496e1750ece3f88d39abd4b7
Patrick L Archibald <Patrick.Archibald@hometelco.com>
2017-04-19 13:34:43 -0400

    Compile prior to launch.

--- tweettooter.sh
+++ tweettooter.sh
@@ -2,4 +2,7 @@
 #
 # Run Tweet Tooter 1000
 #
+rm -rf build/classes/main
+mkdir -p build/classes/main
+javac -cp lib/sikulixapi.jar -d build/classes/main src/main/java/net/pla1/tweettooter/*.java
 java -cp build/classes/main:.:lib/* net.pla1.tweettooter.Bot
