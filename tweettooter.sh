9b7cf20c8f2182098235230b961d15e0dd2908e3
Patrick L Archibald <patrick.archibald@gmail.com>
2017-04-21 08:12:39 -0400

    Added while forever loop.

--- tweettooter.sh
+++ tweettooter.sh
@@ -2,7 +2,10 @@
 #
 # Run Tweet Tooter 1000
 #
-rm -rf build/classes/main
-mkdir -p build/classes/main
-javac -cp lib/sikulixapi.jar -d build/classes/main src/main/java/net/pla1/tweettooter/*.java
-java -cp build/classes/main:.:lib/* net.pla1.tweettooter.Bot
+while true
+do
+  rm -rf build/classes/main
+  mkdir -p build/classes/main
+  javac -cp lib/sikulixapi.jar -d build/classes/main src/main/java/net/pla1/tweettooter/*.java
+  java -cp build/classes/main:.:lib/* net.pla1.tweettooter.Bot
+done
