b19f2ff5e9a4a48d46a9fd5f1fdf318cfa759ef7
Patrick L Archibald <patrick.archibald@gmail.com>
2017-04-21 08:15:15 -0400

    Added logger statements.

--- tweettooter.sh
+++ tweettooter.sh
@@ -4,9 +4,11 @@
 #
 while true
 do
+  logger "$0 Start"
   /usr/bin/git pull
   rm -rf build/classes/main
   mkdir -p build/classes/main
   javac -cp lib/sikulixapi.jar -d build/classes/main src/main/java/net/pla1/tweettooter/*.java
   java -cp build/classes/main:.:lib/* net.pla1.tweettooter.Bot
+  logger "$0 Finish"
 done
